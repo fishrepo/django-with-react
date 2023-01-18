8abce7ac48c820baf6b8f4871092279c212d9143
seafish <seafish5252@gmail.com>
2023-01-19 04:34:26 +0900

    model, admin

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# admin.site.register(Post)
+
+
+# class PostAdmin(admin.ModelAdmin):
+#     pass
+
+
+# admin.site.register(Post, PostAdmin)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'is_public',
+                    'message_length', 'created_at', 'updated_at']
+    list_display_links = ['message']
+    search_fields = ['message']
+    list_filter = ['created_at', 'is_public']
